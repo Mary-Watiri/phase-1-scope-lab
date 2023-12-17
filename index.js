10e4bcd0ba541a4181acbe8f9bbd8c846c765c8c
Mary Watiri <watirimary30@gmail.com>
2023-12-17 13:03:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+ customerName = "bob";
+function upperCaseCustomerName() {
+    if(customerName){
+    customerName = customerName.toUpperCase();
+    }
+
+};
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+const leastFavouriteCustomer = "cake";
+ function changeLeastFavoriteCustomer(){
+    leastFavouriteCustomer = "novel";
+ }
